3ffb8a82a6939101692c73990b973e7e0c037a76
Phil Hickman <phickman7872@gmail.com>
2018-12-21 17:06:24 -0500

    Added User Management along with other fixes and spices.

---
+++ TeamCalendarApp/Controllers/Management/UserManagementController.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamCalendarApp.Data;
+using TeamCalendarApp.Models;
+
+namespace TeamCalendarApp.Controllers.Management
+{
+    public class UserManagementController : Controller
+    {
+        private readonly TeamCalendarDataContext _context;
+
+        public UserManagementController(TeamCalendarDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserManagement
+        public async Task<IActionResult> Index()
+        {
+            var user = User.Identity.Name.Split("\\")[1];
+            var teamCalendarDataContext = _context.Users.Include(u => u.Department);
+
+            return View(await teamCalendarDataContext.ToListAsync());
+        }
+
+        // GET: UserManagement/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Department)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: UserManagement/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId");
+            return View();
+        }
+
+        // POST: UserManagement/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,DepartmentId,Username,FirstName,LastName,EmailAddress,IsManager,IsSiteManager,ReportsTo,UserCreated,DateCreated,UserUpdated,DateUpdated")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", user.DepartmentId);
+            return View(user);
+        }
+
+        // GET: UserManagement/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", user.DepartmentId);
+            return View(user);
+        }
+
+        // POST: UserManagement/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,DepartmentId,Username,FirstName,LastName,EmailAddress,IsManager,IsSiteManager,ReportsTo,UserCreated,DateCreated,UserUpdated,DateUpdated")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId", user.DepartmentId);
+            return View(user);
+        }
+
+        // GET: UserManagement/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Department)
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: UserManagement/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+    }
+}
